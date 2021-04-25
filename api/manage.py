ea23e8ba8665e9ebcec1eb5b424f779a7feb4679
Darkborderman <reastw1234@gmail.com>
2021-04-25 19:58:55 +0800

    refactor: Remove migration code currently

--- api/manage.py
+++ api/manage.py
@@ -1,18 +1,14 @@
 import os
 
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 
-from config import db
 from app import create_app
 
 
 def init_manager():
     config_name = os.environ.get("APP_SETTINGS", "development")
     app = create_app(config_name)
-    Migrate(app, db)
     manager = Manager(app)
-    manager.add_command("db", MigrateCommand)
     manager.run()
 
 
