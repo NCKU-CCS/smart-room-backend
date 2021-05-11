56ddabd5b51ade7c8afc82f950f852fb42666ce8
Darkborderman <reastw1234@gmail.com>
2021-05-12 02:23:21 +0800

    style: Update linting

--- api/utils/start_up.py
+++ api/utils/start_up.py
@@ -1,14 +1,15 @@
-# Have to do that to let service know migration route
-# Note that it applied to all routes, so only need to do this once
 import sys
-sys.path.append('../')
-
 import json
 
 from loguru import logger
 
-from migrations.models import Gateway
 from config import SESSION, REDIS
+# Have to do that to let service know migration route
+# Note that it applied to all routes, so only need to do this once
+# pylint: disable=C0413
+sys.path.append('../')
+from migrations.models import Gateway  # noqa: E402
+# pylint: enable=C0413
 
 
 def init_redis():
