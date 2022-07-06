c0998ea1666da4735ed26e6b609ec46552df35d5
lhleung <grandq33769@gmail.com>
2022-07-07 04:21:36 +0800

    style(lint): make lint

--- api/endpoints/raw/meter_data/resource.py
+++ api/endpoints/raw/meter_data/resource.py
@@ -18,26 +18,39 @@ class RawMeterDataResource(Resource):
     def _set_get_parser(self):
         self.get_parser = reqparse.RequestParser()
         self.get_parser.add_argument("sensor", type=str, required=True, location="values")
-        self.get_parser.add_argument("start", type=lambda x: datetime.strptime(x,'%Y-%m-%dT%H:%M:%S'), required=True, location="values")
-        self.get_parser.add_argument("end", type=lambda x: datetime.strptime(x,'%Y-%m-%dT%H:%M:%S'), required=True, location="values")
+        self.get_parser.add_argument(
+            "start",
+            type=lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%S'),
+            required=True,
+            location="values"
+        )
+        self.get_parser.add_argument(
+            "end",
+            type=lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%S'),
+            required=True,
+            location="values"
+        )
 
     # pylint: disable=R0201
     @USER_AUTH.login_required
     def get(self):
         """Get Latest Sensor Data"""
         args = self.get_parser.parse_args()
-        logger.info(f"[GET Raw Meter Data Request]\n User: {g.account}, Meter: {args['sensor']}, Start: {args['start']}, End: {args['end']}")
+        logger.info(
+            f"[GET Raw Meter Data Request]\n"
+            f"User: {g.account}, Meter: {args['sensor']}, "
+            f"Start: {args['start']}, End: {args['end']}"
+        )
         data = SESSION.query(MeterData).filter(
             MeterData.sensor == args['sensor'],
-            MeterData.created.between(args['start'], args['end']
-        )).order_by(MeterData.created).all()
+            MeterData.created.between(args['start'], args['end'])
+        ).order_by(MeterData.created).all()
 
         if "WATER_DISPENSER" in args['sensor']:
             voltage = NORMAL_VOLTAGE
         else:
             voltage = VOLTAGE
 
-        
         result = [{
             "created": (d.created.replace(tzinfo=timezone.utc).astimezone(tz=TZ)).strftime('%Y-%m-%dT%H:%M:%S'),
             "power": d.current * voltage,
